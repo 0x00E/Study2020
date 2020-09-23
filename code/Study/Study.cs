5cc05f8aafb031e8b2b3db616308648decd8a7fd
qianniancc <qiannian99@gmail.com>
2020-09-23 22:57:15 +0800

    new

--- code/Study/Study.cs
+++ code/Study/Study.cs
@@ -18,10 +18,48 @@ namespace com.qianniancc{
 			short s=12;
 			long l=1234567890;
 			String str="test";
-			print(d);
+			p(d);
+			if(true){
+				p(1);
+			}else{
+				p(3);
+			}
+			int i=1;
+			while(true){
+				p(i);
+				if(++i==10)
+				  break;
+			}
+			int j=1;
+			do{
+				p(j++);
+			}while(j<=10);
+			
+			for(int k=0;k<9;k++)
+			  p(k);
+			  
+			char cxx='z';
+		    switch(cxx){
+		    	case 'z':
+		    	  p(568);
+		    	  break;
+		    	case 'b':
+		    	  p(865);
+		    	  break;
+		    }
+            test:
+            	p(666999);
+            
+            //666
+            //goto test;
+		    
+		    
+			
+		    
+			
         }
         
-        public static void print(Object obj){
+        public static void p(Object obj){
         	Console.WriteLine(obj);
         }
     }
